64b780975cb696bb8898f5a307b97c1b02599002
Theu160c <mahteus2901@yahoo.com>
2025-02-11 13:17:42 +0000

    Criei os printf e scanf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,20 +1,28 @@
 #include <stdio.h>
 
-    int main(){
-        int populacao;
-        float area
-        float pib
-        int numero de pontos turisticos
+int main(){
+    int Populacao;
+    float Area;
+    float PIB;
+    int TURISMO;
 
-        printf("Populacao: \n");
-        scanf("%d", populacao);
-        
+    printf("Digite a populacao: \n");
+    scanf("%d", &Populacao);
+
+    printf("Digite a area por km: \n");
+    scanf("%f", &Area);
+
+    printf("Digite o PIB: \n");
+    scanf("%f", &PIB);
+
+     printf("Digite os pontos turistico: \n");
+    scanf("%d", &TURISMO);
+
+    printf("Populacao: %d \n  Area por km : %f \n");
+    printf("PIB: %f \n TURISMO: %d \n");
+    
+
+    return 0;
 
 
-
-
-
-
-
-
-    }
+}
