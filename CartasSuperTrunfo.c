a5d269d59df8266c01a1364c1a756366c50d6fbf
Theu160c <mahteus2901@yahoo.com>
2025-02-11 13:46:45 +0000

    Concertei erros e finalizei

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -20,7 +20,10 @@ int main(){
      printf("Digite os pontos turistico: \n");
     scanf("%d", &TURISMO);
 
+    printf("Populacao: %d \n - Area por km : %f \n , Populacao, Area");
+    printf("PIB: %f \n - TURISMO: %d \n, PIB, TURISMO");
 
-    printf("Populacao: %d \n - Area por km : %f \n ", Populacao, Area");
-    printf("PIB: %f \n - TURISMO: %d \n", PIB, TURISMO);
+    return 0;
+
     }
+
